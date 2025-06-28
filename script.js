d03aced5afef1e63494ba5b4c8d8ac4177282861
Fael Lucas <fael3678992@gmail.com>
2025-06-28 01:19:15 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+function mostrarMensagem() { 
+    document.getElementById("mensagem").textContent ='"Só se pode alcançar um grande êxito quando nos mantemos fiéis a nós mesmos." Friedrich Nietzsche';
+}
